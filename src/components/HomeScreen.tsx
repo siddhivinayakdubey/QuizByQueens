01ecc34378f1a59f4756694cd64b26e0106513b2
Siddhivinayak Dubey <45829257+siddhivinayakdubey@users.noreply.github.com>
2021-10-24 14:39:22 +0530

    Add files via upload

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,30 +1,50 @@
 import React, {useEffect, useState} from 'react';
 import 'bulma/css/bulma.min.css';
+import '../index.css'
 import Image from './assets/bg7-01.jpg';
 import logo from './assets/logo-02.png';
 import axios from 'axios';
+
+// jyoti is the best
+
 
 
 function HomeScreen() {
 
     const [questions, setQuestions] = useState("Loading");
+    const [userInput , setUserInput] = useState("");
+    const [responseData , setResponseData] = useState("");
+    const [correct , setCorrect] = useState(0);
+    const [wrong , setWrong] = useState(0);
+    const checkAnswer = () => {
+
+        if( userInput === responseData){
+            setCorrect(correct + 1);
+        }
+       else{
+           setWrong(wrong + 1);
+        }
+    }
+
     // @ts-ignore
     useEffect(() => {
         const url = "https://jservice.io/api/random";
         setTimeout(() => {
-            axios.get(url).then((response: { data : any; }) => response.data)
-                .then((data: {
+            axios.get(url).then((response: { data : any; }) => response.data).then((data: {
                     }) => {
-                    console.log(data)
+                console.log(data)
                     // @ts-ignore
-                    setQuestions(data[0].question)
 
+                    setQuestions(data[0].question)
+                    // @ts-ignore
+
+                   setResponseData(data[0].answer);
+                    console.log(responseData);
                 })
         }, 1000);
     }, []);
 
     return (
-
         <section className="hero is-danger is-fullheight " style={{
             backgroundImage: 'url('+Image+')',
             backgroundPosition:"center",
@@ -52,54 +72,48 @@ function HomeScreen() {
                         <div id="navbarMenu" className="navbar-menu">
                             <div className="navbar-end">
                             <span className="navbar-item">
-                                <a className="button is-white is-outlined"style={{
-                                    backgroundColor:"#DBF1EE"
-                                }} href="https://www.queensenglish.co">
+                                <a className="button is-white is-outlined" href="https://www.queensenglish.co">
                                     <span className="icon">
                                         <i className="fa fa-home"></i>
                                     </span>
                                     <span style={{
-                                        color:"black",
+                                        color:"white",
                                         textAlign:"center"
-                                    }}>Website</span>
+                                    }}>QueensEnglish</span>
                                 </a>
                             </span>
-                                <span className="navbar-item" style={{textAlign:"center"}}>
-                                <a className="button is-white is-outlined" style={{
-                                    backgroundColor:"#DBF1EE"
-                                }}href="https://www.facebook.com/TheQueensEnglishUK/">
+                                <span className="navbar-item" style={{textAlign:"center"
+                                }}>
+                                <a className="button is-white is-outlined"  href="https://www.facebook.com/TheQueensEnglishUK/">
                                     <span className="icon">
                                         <i className="fa fa-superpowers"></i>
                                     </span>
                                     <span style={{
-                                        color:"black",
+                                        color:"white",
+
+
                                         textAlign:"center"
                                     }}>Facebook</span>
                                 </a>
                             </span>
                                 <span className="navbar-item">
-                                <a className="button is-white is-outlined" style={{
-                                    backgroundColor:"#DBF1EE"
-                                }}href="#">
+                                <a className="button is-white is-outlined" href="#">
                                     <span className="icon">
                                         <i className="fa fa-book"></i>
                                     </span>
                                     <span style={{
-                                        color:"black",
+                                        color:"white",
                                         textAlign:"center"
                                     }}>LinkedIn</span>
                                 </a>
                             </span>
                                 <span className="navbar-item">
-                                <a className="button is-white is-outlined" style={{
-                                    backgroundColor:"#DBF1EE"
-                                }}
-                                   href="https://znap.link/siddhivinayakdubey">
+                                <a className="button is-white is-outlined" href="https://znap.link/siddhivinayakdubey">
                                     <span className="icon">
                                         <i className="fa fa-github"></i>
                                     </span>
                                     <span style={{
-                                        color:"black",
+                                        color:"white",
                                         textAlign:"center"
                                     }}>Meet Sid</span>
                                 </a>
@@ -134,8 +148,8 @@ function HomeScreen() {
                                     <div className="field is-grouped">
                                         <p className="control is-expanded">
                                             <input className="input" type="text" placeholder="Type your answer here"
-                                                // value={userInput}
-                                                // onChange={e => setUserInput(userInput.push(e.target.value))}
+                                                value={userInput}
+                                                onChange={e => setUserInput(e.target.value)}
                                             />
                                         </p>
                                         <p className="control">
@@ -144,10 +158,19 @@ function HomeScreen() {
                                                 backgroundColor:"#225163"
                                             }}
                                             //TODO: add on click function here
+                                                // @ts-ignore
+                                                onClick={checkAnswer}
                                             >
                                                 Submit
                                             </button>
                                         </p>
+                                    </div>
+                                    <div className="card">
+                                        <footer className="card-footer">
+                                            <a href="#" className="card-footer-item">Correct:{correct} </a>
+
+                                            <a href="#" className="card-footer-item">Wrongs:{wrong} </a>
+                                        </footer>
                                     </div>
                                 </div>
                             </div>
@@ -160,5 +183,7 @@ function HomeScreen() {
         </section>
     )
 }
+
+
 
 export default HomeScreen;
